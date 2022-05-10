91b43625062c824e909ba2b21a463ed23f09a025
adl.alfanh@xl.co.id <adl.alfanh@xl.co.id>
2022-05-10 14:30:33 +0700

    add code check db

--- connection/index.js
+++ connection/index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
-// var db = require('./config/db.config');
-// var bodyParser = require('body-parser');
-// env
+var db = require('./config/db.config');
+var bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+ // env
 var port = process.env.DB_PORT || 3000;
 
-// db.authenticate().then(() =>
-//     console.log('Koneksi berhasil'));
+db.authenticate().then(() =>
+    console.log('Koneksi berhasil'));
 
 // routes
 app.get('/connection', function(req, res){
