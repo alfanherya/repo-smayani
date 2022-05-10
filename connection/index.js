dbc35cb34255f614ace770cf0a6f4c803c58098e
adl.alfanh@xl.co.id <adl.alfanh@xl.co.id>
2022-05-10 13:58:27 +0700

    change code

--- connection/index.js
+++ connection/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var db = require('./config/db.config');
 var bodyParser = require('body-parser');
-
 // env
 var port = process.env.DB_PORT || 3000;
 
@@ -15,6 +14,5 @@ app.get('/connection', function(req, res){
 });
 
 // listen
-
 app.listen(port);
 console.log('Server berjalan di port ' + port);
