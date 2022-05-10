e8dc5d18e76dc73a08f5d0f825ad15fe3120f5a9
adl.alfanh@xl.co.id <adl.alfanh@xl.co.id>
2022-05-10 21:54:19 +0700

    ganti method GET

--- datapendidik/routes/routes.cekdataguru.js
+++ datapendidik/routes/routes.cekdataguru.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const db = require('../config/db.config');
 const controller = require('../controllers/index');
 
-router.get('/cekdataguru', controller.cekdataguru.getdataguru);
+router.post('/cekdataguru', controller.cekdataguru.getdataguru);
 
 module.exports = router;
