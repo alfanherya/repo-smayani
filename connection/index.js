88d4063c232e69b42a713b6b5fa6a187c83fa77d
adl.alfanh@xl.co.id <adl.alfanh@xl.co.id>
2022-05-10 14:17:16 +0700

    rollback call db config

--- connection/index.js
+++ connection/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
-var db = require('./config/db.config');
-var bodyParser = require('body-parser');
+// var db = require('./config/db.config');
+// var bodyParser = require('body-parser');
 // env
 var port = process.env.DB_PORT || 3000;
 
-db.authenticate().then(() =>
-    console.log('Koneksi berhasil'));
+// db.authenticate().then(() =>
+//     console.log('Koneksi berhasil'));
 
 // routes
 app.get('/connection', function(req, res){
